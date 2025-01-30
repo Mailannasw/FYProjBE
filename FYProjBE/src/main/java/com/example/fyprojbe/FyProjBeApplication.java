6a89daf5ccfdbe04d34df12f4f0fc5981edb3ace
Shea <mailannasw@gmail.com>
2025-01-30 16:39:46 +0000

    empty spring project

---
+++ FYProjBE/src/main/java/com/example/fyprojbe/FyProjBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.fyprojbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyProjBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyProjBeApplication.class, args);
+	}
+
+}
---
+++ FYProjBE/src/test/java/com/example/fyprojbe/FyProjBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fyprojbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FyProjBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
