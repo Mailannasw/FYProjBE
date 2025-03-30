3b70b71771c368897e7fda66ee604006abec3ef8
Shea <mailannasw@gmail.com>
2025-03-30 23:08:08 +0100

    added /card/** to paths allowed to be accessed without logging in

--- FYProjBE/src/main/java/com/example/fyprojbe/config/SecurityConfig.java
+++ FYProjBE/src/main/java/com/example/fyprojbe/config/SecurityConfig.java
@@ -38,6 +38,7 @@ public class SecurityConfig {
                     .requestMatchers(
                             "/user/create",
                             "/user/login",
+                            "/card/**",
                             "/cards/**",
                             "/definition/**").permitAll()
                     .anyRequest().authenticated()
